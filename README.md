75f9c262f916857dc1964b6498367a6dcddecd46
Jonas Wagner <boom@confuflux.com>
2017-12-26 19:34:58 -0700

    could work, probably not.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # porygon2-dice
+
+For that time when you need to make important life-decisions like if you should buy this Tesla or invest all your money in ethereum or monero and so on. Rolling a dice helps you with that tremendously. It's either a yes or a no, easy as 1 2 3.
---
+++ dice.go
@@ -0,0 +1,28 @@
+package bot
+
+import (
+	"fmt"
+	"github.com/0x263b/porygon2"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func dice(command *bot.Cmd, matches []string) (msg string, err error) {
+	seed := rand.NewSource(time.Now().UnixNano())
+	rn := rand.New(seed)
+
+	var s []string
+
+	for i := 0; i < 5; i++ {
+		s = append(s, strconv.Itoa(rn.Intn(6)+1))
+	}
+
+    return fmt.Sprintf(strings.Join(s, " ")), nil
+}
+
+func init() {
+	bot.RegisterCommand("^dice$", dice)
+	log.Println("dice plugin loaded")
+}
